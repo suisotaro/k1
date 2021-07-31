99ace9c0f4c2407dcf6f635b7855b9d2fcce5c0e
suisotaro <63042242+suisotaro@users.noreply.github.com>
2021-07-31 09:01:07 +0900

    Create make.js

---
+++ make.js
@@ -0,0 +1,17 @@
+var make = function(url, shorturl){
+  if(url == undefined){
+    return "error"
+  }
+  var geturl = "https://script.google.com/macros/s/AKfycbzQ-y1-v9GIozewAM2ivQ19VJHBxUtrIqT3apm-vrGmt4wJlDbmiVPm_z0FN3DgQtDhsg/exec?url=" + url;
+  if(shorturl != undefined){
+    geturl += "&shorturl=" + shorturl;
+  }
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", geturl);
+  xhr.onreadystatechange = function(){
+    if (xhr.readyState === 4 && xhr.status === 200){
+      return xhr.responseText;
+    }
+  };
+  xhr.send(null);
+}
